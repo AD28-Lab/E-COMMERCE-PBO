a2e736869d2b1eb8b15a6241c08fa96c73d1b7ab
adityatustanda <adityatustanda28@gmail.com>
2023-05-30 17:55:03 +0800

    menambahkan database

--- source/src/main/java/org/example/Main.java
+++ source/src/main/java/org/example/Main.java
@@ -1,51 +1,47 @@
 package org.example;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
-
-import javax.imageio.IIOException;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.util.concurrent.Executor;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost", 8125), 0);
-
-        httpServer.createContext("/user", new userHandler());
-        httpServer.setExecutor(Executors.newSingleThreadExecutor());
-        httpServer.start();
-        System.out.println("Udah connect...");
-
+        try{
+            HttpServer httpserver = HttpServer.create(new InetSocketAddress("localhost", 8125),0);
+            httpserver.createContext("/", new Server());
+//            test
+            httpserver.setExecutor(Executors.newSingleThreadExecutor());
+            httpserver.start();
+            System.out.println( "Listening on port 8125...");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
-    public static class userHandler implements HttpHandler{
+    public static class Connect{
 
-
-        @Override
-        public void handle(HttpExchange exchange) throws IOException {
-            // POST -> create
-            // GET -> read
-            // PUT -> update
-            // DELETE -> delete
-            // CRUD
-
-            if ("GET".equals(exchange.getRequestMethod())) {
-                // send something intresting to user
-                OutputStream outputStream = exchange.getResponseBody();
-                String responseToBeSendBack = "Hello from the other side";
-                exchange.sendResponseHeaders(200, responseToBeSendBack.length());
-
-
-                outputStream.write(responseToBeSendBack.getBytes());
-                outputStream.flush();
-                outputStream.close();
+        public static void connect(){
+            Connection connection = null;
+            String rootPath = System.getProperty("user.dir");
+            try {
+                String url = "jdbc:sqlite:C:\\Users\\USER\\IdeaProjects\\E-COMMERCE-PBO\\ecommerce";
+                connection = DriverManager.getConnection(url);
+                System.out.println("Connection to SQLite has been established");
+            }   catch (SQLException e){
+                    System.out.println(e.getMessage());
+            }   finally {
+                  try {
+                      if(connection != null){
+                            connection.close();
+                      }
+                  }catch (SQLException exception){
+                      System.out.println(exception.getMessage());
+                  }
             }
         }
-
     }
-
 }
